50dacf7d38eb03d0219a02ac4d7c4bbb8ca8a3c8
luanerdy <luan.vini1@gmail.com>
2022-09-01 10:30:46 -0300

    chore: adding prettier

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		environment: 'jsdom',
-	},
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+    },
 })
